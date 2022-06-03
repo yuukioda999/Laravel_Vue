7a8a984803609ead93906e73ce9a71c5c911f5b1
yuukioda999 <yuuki46yuuki@icloud.com>
2022-06-03 14:02:31 +0900

    認証ページコンポーネント作成

--- web/app/Http/Controllers/Auth/LoginController.php
+++ web/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,18 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // ログイン
+    protected function authenticated(Request $request, $user)
+    {
+        return $user;
+    }
+
+    protected function loggedOut(Request $request)
+{
+    // ログアウト
+    $request->session()->regenerate();
+
+    return response()->json();
+}
 }
